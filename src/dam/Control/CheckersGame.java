6a677d8d22716e258893acaf30d4820d276eaea2
Søren Mousten <s164436@student.dtu.dk>
2017-01-09 12:19:50 +0100

    Kan nu fra Main ved at oprette et CheckersGame, som indeholder initilization.

--- src/dam/Control/CheckersGame.java
+++ src/dam/Control/CheckersGame.java
@@ -1,13 +1,51 @@
 package dam.Control;
 
+import dam.abstractions.CheckerPiece;
+import dam.abstractions.IdentifierGenerator;
+import dam.abstractions.LogicBoard;
+import dam.abstractions.Player;
+import dam.graphics.GUIBoard;
+
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Emil Damsbo on 09-01-2017.
  */
 public class CheckersGame {
-    private int BoardSize = 3;
+    private int boardSize;
+    public static CheckerPiece[][] pieces;
 
-    public CheckersGame(int n){
-        this.BoardSize = n;
+    public CheckersGame(int n) {
+        this.boardSize = n;
+        // initialization phase
+        IdentifierGenerator idGen = new IdentifierGenerator();
+        Player player1 = new Player("Player 1", idGen.getNextIdentifier());
+        Player player2 = new Player("Player 2", idGen.getNextIdentifier());
+        Player placeholder = new Player("This guy does not exist", -1);
+        pieces = new CheckerPiece[boardSize][boardSize];
+
+
+        // board creation
+        LogicBoard board = new LogicBoard(pieces, boardSize);
+        board.populate(player1, player2, placeholder);
+        board.printBoard();
+
+
+        GUIBoard temp = new GUIBoard(boardSize, board);
+        //temp.printButtonArraySize();
+
+        // ensures all buttons act properly when clicked
+        temp.fillInAllActionHandlers();
+        // graphics loading
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.add(temp, BorderLayout.CENTER);
+        f.setResizable(false);
+
+        f.pack();
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
     }
 
 }
--- src/test/Main.java
+++ src/test/Main.java
@@ -8,6 +8,9 @@ import dam.Control.CheckersGame;
  * Created by Emil Damsbo on 09-01-2017.
  */
 public class Main {
-    CheckersGame game = new CheckersGame();
+
+    public static void main(String[] args) {
+        CheckersGame game = new CheckersGame(Integer.parseInt(args[0]));
+    }
 
 }
